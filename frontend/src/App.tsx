e2ce3fd8fcf82924d059333cb97de23d54929c12
sontruonghp2004 <tranvutruongson14062004@gmail.com>
2025-11-05 17:09:17 +0700

    fix layout all

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
 // Import các trang
+import GameGuide from "./components/GameGuide";
 import HeroSection from "./components/HeroSection";
 import MiniGame from "./components/MiniGame";
+import AiDisclosurePage from "./pages/AiDisclosurePage";
+import GalleryPage from "./pages/GalleryPagePage";
 import PresentationPage from "./pages/PresentationPage";
 import TimelinePage from "./pages/TimelinePage";
 
@@ -14,10 +17,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HeroSection />} />
+          <Route path="/guide" element={<GameGuide />} />
           <Route path="game" element={<MiniGame />} />
           <Route path="timeline" element={<TimelinePage />} />
+          <Route path="gallery" element={<GalleryPage />} />
+
           <Route path="presentations" element={<PresentationPage />} />
-          {/* <Route path="ai-disclosure" element={<AIDisclosurePage />} /> */}
+          <Route path="ai-disclosure" element={<AiDisclosurePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
